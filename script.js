bcc4e71d29501d169a58ce7f9d62d15ca381c557
Ilaria <128404113+ilariaciavarella@users.noreply.github.com>
2023-04-15 17:26:35 +0200

    Play a game with five rounds

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const rps = ["Rock", "Paper", "Scissors"];
+let singleResult;
 
 // Get computer's selection using random and store it in a variable
 // First get random index of array
@@ -12,62 +13,84 @@ function getComputerChoice() {
     return rps[choiceInd];
 }
 
-//Store computer's choice in variable
-let computerSelection = getComputerChoice();
-
-console.log(computerSelection);
-
 // Get player's selection using prompt and store it in a variable
-let playerSelection = prompt("Rock, paper or scissors?", "");
+function getPlayerChoice() {
+    return prompt("Rock, paper or scissors?", "");
+}
 
-console.log(playerSelection);
 
 // Compare every case (rock vs paper, rock vs scissors, paper vs scissors, rock vs rock, paper vs paper, scissors vs scissors)
 // First check playerSelection 
 function singleRound(computerSelection, playerSelection) {
-    let result;
+    computerSelection = getComputerChoice();
+    playerSelection = getPlayerChoice();
+
     switch (playerSelection.toLowerCase()) {
         case "rock":
             if (computerSelection === "Paper") {
-                result = "You lose! Paper beats Rock";
+                singleResult = "You lose! Paper beats Rock";
             } else if (computerSelection === "Scissors") {
-                result = "You won! Rock beats Scissors";
+                singleResult = "You won! Rock beats Scissors";
             } else {
-                result = "It's a tie! Try again.";
+                singleResult = "It's a tie! Try again.";
             }
             break;
 
         case "paper":
             if (computerSelection === "Scissors") {
-                result = "You lose! Scissors beats Paper";
+                singleResult = "You lose! Scissors beats Paper";
             } else if (computerSelection === "Rock") {
-                result = "You won! Paper beats Rock";
+                singleResult = "You won! Paper beats Rock";
             } else {
-                result = "It's a tie! Try again.";
+                singleResult = "It's a tie! Try again.";
             }
             break;
 
         case "scissors":
             if (computerSelection === "Rock") {
-                result = "You lose! Rock beats Scissors";
+                singleResult = "You lose! Rock beats Scissors";
             } else if (computerSelection === "Paper") {
-                result = "You won! Scissors beats Paper";
+                singleResult = "You won! Scissors beats Paper";
             } else {
-                result = "It's a tie! Try again.";
+                singleResult = "It's a tie! Try again.";
             }
             break;
 
         default:
-            result = "What you entered is not valid, try again.";
+            singleResult = "What you entered is not valid, try again.";
     }
 
-// Return winner
-    return result;
-    console.log(result);
+    // Return winner
+    return singleResult;
 }
 
-singleRound(computerSelection, playerSelection);
-
 // Iterate 5 times
-// Count times each winning
-// Return final winner
+// For each round I want to give a point to whoever wins, at the end of the five rounds I want to compare the points and return the final winner
+function game () {
+    let playerPoints = 0;
+    let computerPoints = 0;
+
+    for (let i = 0; i < 5; i++) {
+        singleRound();
+
+        // Count times each winning
+        if (singleResult.includes("won")) {
+            playerPoints++;
+        } else if (singleResult.includes("lose")) {
+            computerPoints++;
+        }
+        console.log(singleResult);
+        console.log("Score: " + playerPoints + " - " + computerPoints);
+    }
+
+    // Return final winner
+    if (playerPoints > computerPoints) {
+        console.log("You made it! You're the ultimate winner");
+    } else if (playerPoints < computerPoints){
+        console.log("I'm sorry, you'll be luckier next time");
+    } else if (playerPoints == computerPoints) {
+        console.log("No winners or losers this time");
+    }
+}
+
+game();
